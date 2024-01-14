75ef826a32e5454379a4b87fb4a974faee05c61b
user2130981023 <137002272+user2130981023@users.noreply.github.com>
2024-01-14 12:40:35 -0500

    lecture 4 Using Arithmetic Operators, basically math

--- Udemy1/Udemy1/Program.cs
+++ Udemy1/Udemy1/Program.cs
@@ -5,33 +5,50 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-// writeline makes the text print to a new line after that line is printed
-// Write combines the text that is printed on the same line
-// after the comma is what it prints the brackets specify what to print
-// in the brackets ex: {0} {1} if you do {0, 5} it will take up 5 spaces no matter what
-// if you add a {0, -5} it will add the space from the left to the right side of the text
-// line 33 if you take away the space before the quotes end "my int is _" then the myInt variable will be right next to your text because there is no space between them
+// + - / * %
 
-
-
-namespace DeclaringVariables
+namespace UsingArithmeticOperators
 {
     class Program
     {
-        // lecture 3 Displaying Variables in the Console so you can see the text
+        // lecture 4 Using Arithmetic Operators, basically math variables
         static void Main(string[] args)
         {
-            int myInt = 555; // int cannot have decimal
-            double myDouble = 5.5; // double can have decimal
-            string myString = "Hello World"; // a string
-            bool myBool = true; // cam be true or false
-            char myChar = 'A'; // char is short for character
+            int myInt = 5 + 5; // addition
+            Console.WriteLine(myInt);
+            int myInt2 = 5 - 5; // subtraction
+            Console.WriteLine(myInt2);
+            int myInt3 = 5 * 5; // multiply
+            Console.WriteLine(myInt3);
+
+            double myDouble = 45.0 / 2; // adding the 44.0 makes it divide by decimal instead of whole number
+            Console.WriteLine(myDouble);
+
+            double remain = 5 % 4; // divides and gets the remainder after it divides 
+            Console.WriteLine(remain);
 
 
-            Console.WriteLine("The value of my int is {0} and the value of my double os {1}", myInt, myDouble);
+            int result = (2 + 3) * 4; // does multiplication first then adds. Parenthases make it execute that math first
+            Console.WriteLine("The result is {0}", result);
 
-            Console.WriteLine("The value of my int is " + myInt + " and the value of my double is " + myDouble); 
 
+            int myInt4 = myInt + myInt2 + myInt3; // adds all the myInts together
+            Console.WriteLine(myInt4);
+
+
+            int x = 5;
+            x = x + 1; // gets the value of x and adds on to it
+            x += 1;
+            
+            Console.WriteLine("The value of x is {0}", x);
+
+            int y = 5;
+            y++; // add 1 to y
+            Console.WriteLine("The value of y is {0}", y);
+
+            int b = 4;
+            int c = b++; // adds by one if it is in front and if the ++ is behind the variable it doesnt go up
+            Console.WriteLine("The value of b is {0} and the value of c is {1}", b, c);
         }
     }
 }
